31112bc2f6ff53640ad36c3306f416f03db93a33
arthurpcardoso <arthurpcardoso13@gmail.com>
2024-11-26 21:13:51 -0300

    fix link

--- miau-front/app/index.tsx
+++ miau-front/app/index.tsx
@@ -1,21 +1,21 @@
 import { Text, View } from "react-native";
-import { Link } from 'expo-router';
+import { Link } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Link href="/home">Ver Home</Link>
-      <Link href="/login">Ver Login</Link>
-      <Link href="/cadastro">Ver Cadastro</Link>
-      <Link href="/cadastropessoal">Ver Cadastro Pessoal</Link>
-      <Link href="/cadastroanimalfeito">Ver Cadastro Animal Finalizado</Link>
-      <Link href="/cadastroanimal">Ver Cadastro Animal</Link>
-    </View>
-  );
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Link href="/home">Ver Home</Link>
+			<Link href="/login">Ver Login</Link>
+			<Link href="/cadastro">Ver Cadastro</Link>
+			<Link href="/cadastropessoal">Ver Cadastro Pessoal</Link>
+			<Link href="/cadastroanimal">Ver Cadastro Animal</Link>
+			<Link href="/cadastroanimalfeito">Ver Cadastro Animal</Link>
+		</View>
+	);
 }
