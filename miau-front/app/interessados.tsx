5148267739614886f2e78cd1817724797f264fa4
arthurpcardoso <arthurpcardoso13@gmail.com>
2025-02-17 18:52:48 -0300

    logica de interessados, pagina de meus pets ta bugada ainda

--- miau-front/app/interessados.tsx
+++ miau-front/app/interessados.tsx
@@ -1,25 +1,75 @@
 import { useState } from "react";
-import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, Modal } from "react-native";
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    ScrollView,
+    TouchableOpacity,
+    Modal,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { getUser } from "@/services/users";
+import { useEffect } from "react";
+import { ActivityIndicator } from "react-native";
+import { useSession } from "@/services/auth";
+import { createChat } from "@/services/chatservice";
 
-export default function Interessados() {
-    const navigation = useNavigation();
-    const [selectedUser, setSelectedUser] = useState(null);
+export default function Interessados({ route }: any) {
+    const interessados = route.params.interessados;
+    const animalID = route.params.animalID;
+    const navigation = useNavigation<any>();
+    const user = useSession().user;
+
+    const [selectedUser, setSelectedUser] = useState<{
+        id: string;
+        name: string;
+        image_url: string;
+    } | null>(null);
     const [modalVisible, setModalVisible] = useState(false);
+    const [users, setUsers] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
 
-    const exemplos = [
-        { id: "1", name: "João Silva", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQCOmPsstmko3MHpax6444mjeEM_E-5Cd60Q&s" },
-        { id: "2", name: "Maria Oliveira", photo: "https://www.google.com/imgres?q=foto%20viuva%20negra&imgurl=https%3A%2F%2Fstatic.wikia.nocookie.net%2Funiversocinematograficomarvel%2Fimages%2F0%2F05%2FNat_Ultron.jpg%2Frevision%2Flatest%2Fscale-to-width%2F360%3Fcb%3D20211229224652%26path-prefix%3Dpt&imgrefurl=https%3A%2F%2Funiversocinematograficomarvel.fandom.com%2Fpt-br%2Fwiki%2FVi%25C3%25BAva_Negra&docid=NcyFWzhsR7zORM&tbnid=8qKdEmdKVXtcgM&vet=12ahUKEwjGs8Su7LuLAxX6qZUCHcXfJsAQM3oECBsQAA..i&w=360&h=510&hcb=2&ved=2ahUKEwjGs8Su7LuLAxX6qZUCHcXfJsAQM3oECBsQAA" },
-        { id: "3", name: "Carlos Santos", photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wallpapers.com%2Fimagens-do-capitao-america-da-marvel&psig=AOvVaw0dwkoWhrnlIRehwn1X6W27&ust=1739371109874000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCODIlLvsu4sDFQAAAAAdAAAAABAE" }
-    ];
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const userPromises = interessados.map((id: string) => getUser(id));
+            const usersData = await Promise.all(userPromises);
+            setUsers(usersData);
+            setLoading(false);
+        };
+
+        fetchUsers();
+    }, [interessados]);
+
+    const handleChat = async (userId: string) => {
+        const chat = await createChat(user.uid, userId, animalID).then(() => {
+            navigation.navigate("Meus Chats");
+        });
+    };
+
+    if (loading) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
 
     return (
         <View style={styles.container}>
             <ScrollView>
-                {exemplos.map((user) => (
+                {users.map((user) => (
                     <View key={user.id} style={styles.userCard}>
-                        <TouchableOpacity onPress={() => { setSelectedUser(user); setModalVisible(true); }}>
-                            <Image source={{ uri: user.photo }} style={styles.userImage} />
+                        <TouchableOpacity
+                            onPress={() => {
+                                setSelectedUser(user);
+                                setModalVisible(true);
+                            }}
+                        >
+                            <Image
+                                source={{ uri: user.photo }}
+                                style={styles.userImage}
+                            />
                         </TouchableOpacity>
                         <Text style={styles.userName}>{user.name}</Text>
                     </View>
@@ -29,14 +79,27 @@ export default function Interessados() {
             <Modal visible={modalVisible} transparent animationType="fade">
                 <View style={styles.modalContainer}>
                     <View style={styles.modalContent}>
-                        <Text style={styles.modalTitle}>{selectedUser?.name}</Text>
-                      <TouchableOpacity style={styles.modalButton} onPress={() => navigation.navigate("Chat", { userId: selectedUser?.id })}>
+                        <Text style={styles.modalTitle}>
+                            {selectedUser?.name}
+                        </Text>
+                        <TouchableOpacity
+                            style={styles.modalButton}
+                            onPress={() =>
+                                handleChat(selectedUser?.id as string)
+                            }
+                        >
                             <Text>Abrir Chat</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.modalButton} onPress={() => setModalVisible(false)}>
+                        <TouchableOpacity
+                            style={styles.modalButton}
+                            onPress={() => setModalVisible(false)}
+                        >
                             <Text>Excluir</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.modalButton} onPress={() => setModalVisible(false)}>
+                        <TouchableOpacity
+                            style={styles.modalButton}
+                            onPress={() => setModalVisible(false)}
+                        >
                             <Text>Cancelar</Text>
                         </TouchableOpacity>
                     </View>
@@ -51,8 +114,26 @@ const styles = StyleSheet.create({
     userCard: { flexDirection: "row", alignItems: "center", marginBottom: 20 },
     userImage: { width: 50, height: 50, borderRadius: 25, marginRight: 10 },
     userName: { fontSize: 16 },
-    modalContainer: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "rgba(0,0,0,0.5)" },
-    modalContent: { backgroundColor: "white", padding: 20, borderRadius: 10, width: 300, alignItems: "center" },
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0,0,0,0.5)",
+    },
+    modalContent: {
+        backgroundColor: "white",
+        padding: 20,
+        borderRadius: 10,
+        width: 300,
+        alignItems: "center",
+    },
     modalTitle: { fontSize: 18, fontWeight: "bold", marginBottom: 10 },
-    modalButton: { padding: 10, width: "100%", alignItems: "center", marginTop: 10, backgroundColor: "#ddd", borderRadius: 5 }
+    modalButton: {
+        padding: 10,
+        width: "100%",
+        alignItems: "center",
+        marginTop: 10,
+        backgroundColor: "#ddd",
+        borderRadius: 5,
+    },
 });
